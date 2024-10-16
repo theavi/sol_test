376f26b5e6befab6fc5cf880d9df234eac7910c7
Avinash <khadsanavinash01@gmail.com>
2024-10-16 06:53:47 +0530

    SOL-06 Dynamic publishing with bindingHeaders.tartgetDestination

--- src/main/java/com/avishtech/sol_test/DynamicPublishing.java
+++ src/main/java/com/avishtech/sol_test/DynamicPublishing.java
@@ -2,12 +2,15 @@ package com.avishtech.sol_test;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.binder.BinderHeaders;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 @Component
 public class DynamicPublishing {
@@ -20,11 +23,25 @@ public class DynamicPublishing {
             logger.info("solace_correlationId is : {}", v.getHeaders().get("solace_correlationId"));
 
             String cId = (String) v.getHeaders().get("solace_correlationId");
-            if(cId==null){
-                cId=Integer.toString(1);
+            if (cId == null) {
+                cId = Integer.toString(1);
             }
-            String topic="solace/cid/".concat(cId);
-            sb.send(topic,v.getPayload());
+            String topic = "solace/cid/".concat(cId);
+            sb.send(topic, v.getPayload());
+        };
+    }
+
+    @Bean
+    public Function<Message<String>, Message<String>> bindingHeader() {
+        return v -> {
+            logger.info("Received : {}", v.getPayload());
+            logger.info("solace_correlationId :{}", v.getHeaders().get("solace_correlationId"));
+            String cId = (String) v.getHeaders().get("solace_correlationId");
+            if (cId == null) {
+                cId = Integer.toString(1);
+            }
+            String topic = "spring/cloud/bindingHeader/".concat(cId);
+            return MessageBuilder.withPayload(v.getPayload()).setHeader(BinderHeaders.TARGET_DESTINATION, topic).build();
         };
     }
 }
